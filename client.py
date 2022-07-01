e04d10d3badb991a60113998e42e0279faf19e26
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 11:08:16 +0900

    fix breake stash

--- client.py
+++ client.py
@@ -2,13 +2,15 @@ import aiohttp
 import asyncio
 
 
-async with session.ws_connect('http://example.org/ws') as ws:
+async with session.ws_connect('http://localhost:8080/ws') as ws:
+    print(ws)
     async for msg in ws:
         if msg.type == aiohttp.WSMsgType.TEXT:
             if msg.data == 'close cmd':
                 await ws.close()
                 break
             else:
-                await ws.send_str(msg.data + '/answer')
+                await ws.send_str('hello' + '/answer')
         elif msg.type == aiohttp.WSMsgType.ERROR:
             break
+
