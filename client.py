31e827c0a97b96bbca88ce733591c5a8c405afd4
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 11:05:59 +0900

    remove client

---
+++ client.py
@@ -0,0 +1,14 @@
+import aiohttp
+import asyncio
+
+
+async with session.ws_connect('http://example.org/ws') as ws:
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close cmd':
+                await ws.close()
+                break
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            break
