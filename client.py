835e17152385b8729ef699bb6b2a042530a9ee93
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 11:07:24 +0900

    using websockets connect in client

--- client.py
+++ client.py
@@ -2,10 +2,13 @@ import aiohttp
 import asyncio
 
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://httpbin.org/get') as resp:
-            print(resp.status)
-            print(await resp.text())
-
-asyncio.run(main())
+async with session.ws_connect('http://example.org/ws') as ws:
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close cmd':
+                await ws.close()
+                break
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            break
