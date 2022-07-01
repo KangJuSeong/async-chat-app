907d556992c54faf1af08ffc0ffa13aea5b51089
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 17:08:05 +0900

    complete single room and multi user chat (basic code)

--- client.py
+++ client.py
@@ -1,16 +1,29 @@
-import aiohttp
+from aioconsole import ainput
+import aiohttp   
 import asyncio
 
 
-async with session.ws_connect('http://localhost:8080/ws') as ws:
-    print(ws)
-    async for msg in ws:
-        if msg.type == aiohttp.WSMsgType.TEXT:
-            if msg.data == 'close cmd':
-                await ws.close()
-                break
-            else:
-                await ws.send_str('hello' + '/answer')
-        elif msg.type == aiohttp.WSMsgType.ERROR:
-            break
+async def send_message(ws, user):
+    while True:
+        message = await ainput()
+        await ws.send_json({'user': user, 'message': message})
 
+async def receive_message(ws):
+    async for msg in ws:
+        msg_json = msg.json()
+        print(f">>>{msg_json['user']}: {msg_json['message']})")
+
+async def main(user):
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect('http://localhost:8080/ws') as ws:
+            await ws.send_str(user)
+            print(await ws.receive_str())
+         
+            send_message_task = asyncio.create_task(send_message(ws, user))
+            receive_message_task = asyncio.create_task(receive_message(ws))
+            done, pending = await asyncio.wait(
+                    [receive_message_task, send_message_task], return_when=asyncio.FIRST_COMPLETED,
+            )
+
+user = input('Input name : ')
+asyncio.run(main(user))
