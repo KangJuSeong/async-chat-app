f9329f028832de9696657f38c95d707120dba04e
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 10:44:07 +0900

    using websockets

--- server.py
+++ server.py
@@ -2,10 +2,23 @@ from aiohttp import web
 import asyncio
 
 
-async def hello(request):
-    return web.Response(text='Hello, World')
-    
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.sedn_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print("ws connection closed with exception %s" %ws.exception())
+
+    print("websocket, connection closed")
+
+    return ws
+
 app = web.Application()
-app.add_routes([web.get('/', hello)])
-web.run_app(app)
+app.add_routes([web.get('/ws', websocket_handler)])
 
