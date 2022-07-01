a73a78dd9b31e5374d03fb89e95eb71b61c37208
KangJuSeong <wntjd3686@gmail.com>
2022-07-01 11:02:39 +0900

    fix code

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ async def websocket_handler(request):
     await ws.prepare(request)
 
     async for msg in ws:
+        print(ws)
         if msg.type == aiohttp.WSMsgType.TEXT:
             if msg.data == 'close':
                 await ws.close()
             else:
-                await ws.sedn_str(msg.data + '/answer')
+                await ws.send_str(msg.data + '/answer')
         elif msg.type == aiohttp.WSMsgType.ERROR:
             print("ws connection closed with exception %s" %ws.exception())
 
